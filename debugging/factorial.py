1759f7a7ce0222f6f8fb55ca95e755b189bf94a2
LouisManchon <10578@holbertonstudents.com>
2025-05-07 14:56:19 +0200

    fix space

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -5,7 +5,7 @@ def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n -= 1
+        n -= 1  # ✅ Tu as ajouté cette ligne, c’est ce qui manquait
     return result
 
 f = factorial(int(sys.argv[1]))
